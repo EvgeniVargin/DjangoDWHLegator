1915ebc142f314b7af43e93d1a8c99119b158e05
EvgeniVargin <evgeni.vargin@gmail.com>
2020-01-26 19:27:10 +0500

    Django DWH Legator first commit

---
+++ DWHLegator/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DwhlegatorConfig(AppConfig):
+    name = 'DWHLegator'
---
+++ DWHLegator/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Employee(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    key = models.CharField(max_length=256)
+    name = models.CharField(max_length=1000)
+    age = models.BigIntegerField()
+    position = models.CharField(max_length=256)
+    salary = models.DecimalField(max_digits=8,decimal_places=2)
+    bonus = models.DecimalField(max_digits=3,decimal_places=2)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.safe()
+
+    def __str__(self):
+        return """<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>"""%(self.key,self.name,self.age,self.position,self.salary,self.bonus)
